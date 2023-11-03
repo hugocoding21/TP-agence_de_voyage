6dec37c2e5aed5ac550a1e2894ff951efbac6fbe
ilyes-hamed <ilyeshamed92@gmail.com>
2023-11-03 11:55:11 +0100

    fini

--- src/contact/contact.php
+++ src/contact/contact.php
@@ -1,86 +1,90 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <link rel="stylesheet" href="contact.css" />
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../header-footer/css/header.css">
+
+<head>
+  <link rel="stylesheet" href="contact.css" />
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="../header-footer/css/header.css">
   <link rel="stylesheet" href="../header-footer/css/footer.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <title>Contact</title>
-  </head>
-  <body>
+  <title>Contact</title>
+</head>
+
+<body>
   <header>
-        <?php include_once "../header-footer/header.php"; ?>
-      </header>
-    <div class="page">
-     
-      <div class="bloc1">
-        <img id="imgn1" src="https://picsum.photos/600/200" alt="contact" />
-        <div class="text">
-          <h1>Contact</h1>
-        </div>
-      </div>
+    <?php include_once "../header-footer/header.php"; ?>
+  </header>
+  <div class="page">
 
-      <div class="bloc2">
-        <div class="form">
-          <div class="form1">
-            <label for="|">
-              Firstname : <br />
-              <input type="text" name="" id="" />
-            </label>
-            <label for="">
-              Lastname : <br />
-              <input type="text" name="" id="" />
-            </label>
-            <label for="email">
-              Email : <br />
-              <input type="text" name="" id="" />
-            </label>
-            <label>
-              Nearest agency : <br />
-              <input type="text" name="" id="" />
-            </label>
-          </div>
-
-          <div class="blockmsg">
-            <label for="msg">
-              Message : <br />
-              <input id="msg" type="text" name="" id="" />
-            </label>
-          </div>
-          <div class="btn2">
-            <button id="button2" >Send</button>
-          </div>
-        </div>
-        <div class="info">
-          <div class="adresse">
-            <h2>Adresse</h2>
-            <p>
-              15 rue de la blabla <br />
-              75166 , Quelque-part <br />
-              FRANCE <br />
-              -33 7778888 <br />
-            </p>
-            <p id="other">other adresses</p>
-          </div>
-          <div class="information">
-            <h2>Information</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum,
-              culpa nulla eveniet voluptatem vero odit iste voluptatibus
-              quibusdam deserunt iure?
-            </p>
-          </div>
-        </div>
+    <div class="bloc1">
+      <img id="imgn1" src="https://picsum.photos/600/200" alt="contact" />
+      <div class="text">
+        <h1>Contact</h1>
       </div>
-      
     </div>
-    <header>
-      <?php include_once "../header-footer/footer.php"; ?>
-    </header>
+
+    <div class="bloc2">
+      <form method="POST" action="../verifier_contact/verifier_contact.php" class="form">
+        <div class="form1">
+          <label for="|">
+            Firstname : <br />
+            <input type="text" name="nom" id="" required />
+          </label>
+          <label for="">
+            Lastname : <br />
+            <input type="text" name="prenom" id="" required />
+          </label>
+          <label for="mail">
+            Email : <br />
+            <input type="email" name="mail" id="" required />
+          </label>
+          <label>
+            Nearest agency : <br />
+            <input type="text" name="agency" id="" required />
+          </label>
+        </div>
+
+        <div class="blockmsg">
+          <label for="msg">
+            Message : <br />
+            <input id="msg" type="text" name="message" id="" required />
+          </label>
+        </div>
+        <div class="btn2">
+          <button id="button2" type="submit" name="submit">Send</button>
+        </div>
+      </form>
+
+      <div class="info">
+        <div class="adresse">
+          <h2>Adresse</h2>
+          <p>
+            15 rue de la blabla <br />
+            75166 , Quelque-part <br />
+            FRANCE <br />
+            -33 7778888 <br />
+          </p>
+          <p id="other">other adresses</p>
+        </div>
+        <div class="information">
+          <h2>Information</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum,
+            culpa nulla eveniet voluptatem vero odit iste voluptatibus
+            quibusdam deserunt iure?
+          </p>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  <header>
+    <?php include_once "../header-footer/footer.php"; ?>
+  </header>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
-  </body>
+</body>
+
 </html>
---
+++ src/verifier_contact/verifier_contact.php
@@ -0,0 +1,44 @@
+<?php
+
+if (!empty($_POST)) {
+
+    if (
+        isset($_POST["nom"], $_POST["prenom"], $_POST["mail"], $_POST['agency'], $_POST["message"]) &&
+        !empty($_POST["nom"]) && !empty($_POST["prenom"]) && !empty($_POST["mail"]) && !empty($_POST["agency"]) && !empty($_POST["message"])
+    ) {
+
+        $nom = ucwords(strtolower(htmlspecialchars($_POST["nom"])));
+
+        $prenom = ucwords(strtolower(htmlspecialchars($_POST["prenom"])));
+
+        $mail = strtolower(htmlspecialchars($_POST["mail"]));
+
+        $choix = htmlspecialchars($_POST["agency"]);
+
+        $message = htmlspecialchars($_POST["message"]);
+
+        require_once "../../bd_sql/config.inc.php";
+
+        $sql = "INSERT INTO `fomulaire`(`first_name`,`last_name`,`email`,`Nearest agency`,`message`) VALUE (:c_nom, :c_prenom, :c_mail, :c_choix, :c_messages)";
+
+        $query = $db->prepare($sql);
+
+        $query->bindValue(":c_nom", $nom, PDO::PARAM_STR);
+
+        $query->bindValue(":c_prenom", $prenom, PDO::PARAM_STR);
+
+        $query->bindValue(":c_mail", $mail, PDO::PARAM_STR);
+
+        $query->bindValue(":c_choix", $choix, PDO::PARAM_STR);
+
+        $query->bindValue(":c_messages", $message, PDO::PARAM_STR);
+        
+        header('location: ../contact/contact.php');
+
+        if (!$query->execute()) {
+            die("Une erreur est survenue");
+        }
+    } else {
+        echo "echec de l'envoi";
+    }
+}
